792a1c0dc68755a21d796d5ec0ce7fc333948af2
manima <manimmaa93@gmail.com>
2025-11-26 11:00:14 +0200

    implement Celsius-Fahrenheit conversion logic

--- func.cpp
+++ func.cpp
@@ -1,1 +1,11 @@
 #include "func.h"
+
+double celsius_to_fahrenheit(double celsius)
+{
+    return celsius * (9.0 / 5.0) + 32;
+}
+
+double fahrenheit_to_celsius(double fahrenheit)
+{
+    return (fahrenheit - 32) * (5.0 / 9.0);
+}
--- func.h
+++ func.h
@@ -1,2 +1,7 @@
 #pragma once
 #include <string>
+
+using namespace std;
+
+double celsius_to_fahrenheit(double celsius);
+double fahrenheit_to_celsius(double fahrenheit);
